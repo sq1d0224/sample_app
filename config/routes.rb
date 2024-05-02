45537607f54c594d457aeb3a81913ad56c9c0ee2
EC2 Default User <ec2-user@ip-172-31-28-52.ap-northeast-1.compute.internal>
2024-05-02 13:27:16 +0000

    [Start]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
 end
