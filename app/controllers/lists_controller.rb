693c8a684407e61ea335eb466f887367e490b9f7
sq1d0224 <isho05010224@gmail.com>
2024-05-05 05:00:46 +0000

    [Add]フラッシュメッセージの実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,9 +11,12 @@ class ListsController < ApplicationController
     # 2．データをデータベースに保存するためのsaveメソッド実行
     if @list.save
       # 3．フラッシュメッセージを定義し、詳細画面へリダイレクト
-      flash[:notice] = "投稿に成功しました"
+      flash[:notice] = "投稿に成功しました。"
       redirect_to list_path(@list.id)
     else
+      # 4．フラッシュメッセージを定義し、new.html.erbを描画する
+      # flash.nowでフラッシュメッセージを定義する
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
